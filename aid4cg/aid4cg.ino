ed6536bd1a9ed82c365ab0e58721c1923973ab73
Arulu Autumns <arulu@gearhaus.net>
2020-03-30 04:49:57 -0700

    added new sketch Apparently the .ino specified in arduino.json acts as the "main" file

---
+++ aid4cg/aid4cg.ino
@@ -0,0 +1,29 @@
+/** Aid4CG Prototype
+ * Goals: 
+ *  1 Hardcoded hotkeys
+ *  2 Press and Release keys in sync with button
+ *  3 Perform useful work in a 3d modeling app
+ * */
+
+#include "Keyboard.h"
+
+
+void setup()
+{
+  
+}
+void loop()
+{
+  
+  //check pins
+  // start keyboard if any input is needed3. I am now more familiar with how creating a 'keyboard' type device works, and can avoid this issue now
+
+  
+  // if a pin changed state
+  //  start keypress if it went hi->lo
+  //  end keypress if it went lo->hi
+
+  // end keyboard if all pins are hi
+
+}
+
